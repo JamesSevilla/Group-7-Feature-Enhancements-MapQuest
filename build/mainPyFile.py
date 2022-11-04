5e3baf5c726b25a9e765b7f7ca016f1c7f919ae0
JMAlcausin <111330826+JMAlcausin@users.noreply.github.com>
2022-11-04 13:28:46 +0000

    Update mainPyFile.py
    Added Meters Metric
    
    Needs Front End Adjustments

--- build/mainPyFile.py
+++ build/mainPyFile.py
@@ -133,7 +133,85 @@ def mapQuestMiles():
         for each in json_data["route"]["legs"][0]["maneuvers"]:
             i+=1
             #Print the direction steps
-            direction = Label(outputBox,text=((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61 * 0.62137119) + " Miles)")))
+            direction = Label(outputBox,text=((each["narrative"])))
+            direction.grid(row=i,column=1)
+        
+    elif json_status == 402:
+        #Divider
+        astLine = Label(outputBox,text="**********************************************")
+        astLine.grid(row=8,column=1)
+        #Print Status COde
+        statusCode = Label(outputBox,text=("Status Code: " + str(json_status) + "; Invalid user inputs for one or bothlocations."))
+        statusCode.grid(row=9,column=1)
+        #Divider
+        astLine1 = Label(outputBox,text="**********************************************")
+        astLine1.grid(row=10,column=1)
+    elif json_status == 611:
+        #Divider
+        astLine = Label(outputBox,text="**********************************************")
+        astLine.grid(row=8,column=1)
+        #Print Status COde
+        statusCode = Label(outputBox,text=("Status Code: " + str(json_status) + "; Missing an entry for one or bothlocations."))
+        statusCode.grid(row=9,column=1)
+        #Divider
+        astLine1 = Label(outputBox,text="**********************************************")
+        astLine1.grid(row=10,column=1)
+    else:
+        #Divider
+        astLine = Label(outputBox,text="**********************************************")
+        astLine.grid(row=8,column=1)
+        #Print Status COde
+        statusCode = Label(outputBox,text=("For Status Code: " + str(json_status) + "; Refer to:"))
+        statusCode.grid(row=9,column=1)
+        urlHelp = Label(outputBox,text="https://developer.mapquest.com/documentation/directions-api/status-codes")
+        urlHelp.grid(row=10,column=1)
+        #Divider
+        astLine1 = Label(outputBox,text="**********************************************")
+        astLine1.grid(row=11,column=1)
+    #Close
+    closeButton = Button(outputBox, text="Close", padx=50, command=outputBox.destroy)
+    closeButton.grid(row=i+1,column=1)
+#Main Function of the program with Meters
+def mapQuestMeters():
+    outputBox = Toplevel()
+    #Api and key for URL Access
+    main_api = "http://www.mapquestapi.com/directions/v2/route?"
+    key = "ptA1mBi4uz4PZtAhPot62IBhPZEqFd6j"
+    url = main_api + urllib.parse.urlencode({"key":key, "from":entry_1.get(), "to":entry_2.get()})
+    #Print URL Used
+    urlLabel = Label(outputBox, text=("URL ", (url)))
+    urlLabel.grid(row=7,column=1)
+
+    json_data = requests.get(url).json()
+    json_status = json_data["info"]["statuscode"]
+    if json_status == 0:
+        #Print if Successful route Call
+        successLabel = Label(outputBox,text=("API Status: " + str(json_status) + " = A successful route call.\n"))
+        successLabel.grid(row=8,column=1)
+        #Divider
+        equalLine = Label(outputBox,text="=============================================")
+        equalLine.grid(row=9,column=1)
+        #Print the Original Position
+        dirFrom = Label(outputBox,text=("Directions from " + (entry_1.get()) + " to " + (entry_2.get())))
+        dirFrom.grid(row=10,column=1)
+        #Print the Trip Duration
+        tripDuration = Label(outputBox, text=("Trip Duration: " + (json_data["route"]["formattedTime"])))
+        tripDuration.grid(row=11,column=1)
+        #Print the Kilometers
+        kilometers = Label(outputBox, text=("Meters: " + str("{:.2f}".format(json_data["route"]["distance"] * 1.6 * 1000))))
+        kilometers.grid(row=12, column=1)
+        #Print the Fuel Cost
+        fuelCost = Label(outputBox, text=("Estimated Fuel Cost: ₱" + str("{:.2f}".format(((json_data["route"]["distance"])/25)*268))))
+        fuelCost.grid(row=13, column=1)
+        #Divider
+        equalLine1 = Label(outputBox,text="=============================================")
+        equalLine1.grid(row=14,column=1)
+        #Initialize i for looping
+        i=15
+        for each in json_data["route"]["legs"][0]["maneuvers"]:
+            i+=1
+            #Print the direction steps
+            direction = Label(outputBox,text=((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61*1000) + " Meters)")))
             direction.grid(row=i,column=1)
         
     elif json_status == 402:
@@ -209,6 +287,10 @@ button_image_M = PhotoImage(file=relative_to_assets("button_3.png"))
 button_M = Button(image=button_image_M,borderwidth=0,highlightthickness=0,command=mapQuestMiles,relief="flat")
 button_M.place(x=378.0,y=406.0, width=153.0,height=37.0)
 
+#View Button
+button_image_Me = PhotoImage(file=relative_to_assets("button_3.png"))
+button_Me = Button(image=button_image_Me,borderwidth=0,highlightthickness=0,command=mapQuestMeters,relief="flat")
+button_Me.place(x=539.0,y=406.0, width=153.0,height=37.0)
 
 
 #Exit Button
